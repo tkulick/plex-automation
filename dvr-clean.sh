ab6985a0d11485e297ec7fec2e58ef190771b9bd
Tory Kulick <tory@kulick.io>
2018-02-03 08:15:56 -0500

    Put into the right directory and removed source files afterwards.

--- dvr-clean.sh
+++ dvr-clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Migrate DVR content every night to NAS
 
-cp -R /var/lib/plexmediaserver/Library/DVR/* /mnt/Video/
+cp -R /var/lib/plexmediaserver/Library/DVR/* /mnt/Video/TV\ Shows/
+rm -rf /var/lib/plexmediaserver/Library/DVR/*
+
 
